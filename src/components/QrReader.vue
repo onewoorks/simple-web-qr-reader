b80dcc8c0da09430f60bd2573923157f56db11f2
onewoorks <irwanbibrahim@onewoorks.com>
2019-10-28 18:28:38 +0800

    tune

--- src/components/QrReader.vue
+++ src/components/QrReader.vue
@@ -1,72 +1,42 @@
 <template>
-  <div>
+  <div :class="{ 'fullscreen': fullscreen }" 
+      ref="wrapper" @fullscreenchange="onFullscreenChange">
+      <qrcode-reader class="qrreader">
+        <div>
+          <img src="img/logo.png" alt="nsummit logo" /> 
+        </div>
+      </qrcode-reader>
     <qrcode-stream @decode="onDecode" @init="onInit" />
-    <div class="d-none">
-      <div>{{ customer.message }}</div>
-      <table aria-describedby="" class="table table-bordered">
-        <tbody>
-          <tr>
-            <th scope="col">Name</th>
-            <td>{{ customer.name }}</td>
-            <td rowspan="4" style="vertical-align:middle">
-              Sitting Position
-              <div>{{ customer.sitting_zone }}</div>
-            </td>
-          </tr>
-          <tr>
-            <th scope="col">Email</th>
-            <td>{{ customer.email }}</td>
-          </tr>
-          <tr>
-            <th scope="col">Phone Number</th>
-            <td>{{ customer.phone_number }}</td>
-          </tr>
-          <tr>
-            <th scope="col">Register Date</th>
-            <td>{{ customer.register_date }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-
-    <div class="d-flex hide">
-      <button type="button" class="btn btn-primary btn-block d-none" @click="onConfirm">Confirm</button>
-      <button type="button" class="btn btn-default d-none">Cancel</button>
-    </div>
-
-    <lottie-animation
-                      path="https://assets7.lottiefiles.com/packages/lf20_Vwcw5D.json"
-                      :loop="false"
-                      :autoPlay="true"
-                      :loopDelayMin="2.5"
-                      :loopDelayMax="5"
-                      :speed="1"
-                      :width="256"
-                      :height="256"
-                  />
-
-  <a href="#myModal" role="button" class="d-none btn btn-primary" data-toggle="modal" ref="ticketModal">Launch modal</a>
+  <a 
+    href="#myModal" 
+    role="button" 
+    class="d-none btn btn-primary" 
+    data-backdrop="static" data-keyboard="false"
+    data-toggle="modal" 
+    ref="ticketModal">Launch modal</a>
     <div class="modal" id="myModal" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header model-header-success text-center">
-                <h5 class="modal-title">INFORMATION</h5>
+                <h5 class="modal-title">SITTING ZONE : {{ customer.sitting_zone }}</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">×</span>
                 </button>
             </div>
             <div class="modal-body p-4" id="result">
-                    
-                    <div>
-                      Attendee Information
-                    </div>
+                    <div style='font-weight:bold'>{{ customer.name }}</div>
+                    <div>{{ customer.phone_number }}</div>
+                    <div>{{ customer.email }}</div>
+                    <div>{{ customer.register_date }}</div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-block btn-success" data-dismiss="modal">CONFIRM</button>
+                  <button v-if="customer.valid == 1" type="button" class='btn btn-warning btn-block'>{{ customer.message }}</button>
+                  <button v-else type="button" @click="onConfirm" class="btn btn-block btn-success" data-dismiss="modal">CONFIRM</button>
+                </div>
+                
             </div>
         </div>
-    </div>
-</div>  
+    </div> 
   </div>
 
   
@@ -74,23 +44,63 @@
 
 <script>
 import Axios from "axios";
-import { QrcodeStream } from "vue-qrcode-reader";
+import { QrcodeStream, QrReader } from "vue-qrcode-reader";
 import LottieAnimation from '../../node_modules/lottie-vuejs/src/LottieAnimation'
 
 export default {
   name: "QrReader",
   components: {
     QrcodeStream,
+    QrReader,
     LottieAnimation
   },
   data: function() {
     return {
-      customer: {}
+      customer: {},
+      fullscreen: true
     };
   },
   methods: {
+    onFullscreenChange(event) {
+      // This becomes important when the user doesn't use the button to exit
+      // fullscreen but hits ESC on desktop, pushes a physical back button on
+      // mobile etc.
+
+      this.fullscreen = document.fullscreenElement !== null
+    },
+
+    requestFullscreen() {
+      const elem = this.$refs.wrapper
+
+      if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.mozRequestFullScreen) { /* Firefox */
+        elem.mozRequestFullScreen();
+      } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        elem.webkitRequestFullscreen();
+      } else if (elem.msRequestFullscreen) { /* IE/Edge */
+        elem.msRequestFullscreen();
+      }
+    },
+
+    exitFullscreen() {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.mozCancelFullScreen) { /* Firefox */
+        document.mozCancelFullScreen();
+      } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+        document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) { /* IE/Edge */
+        document.msExitFullscreen();
+      }
+    },
+
+    logErrors (promise) {
+      promise.catch(console.error)
+    },
     onDecode(result) {
       this.reader = result;
+      this.getResult(result)
     },
     onConfirm() { 
       var customer_data = this.customer
@@ -105,6 +115,23 @@ export default {
         "qr_code"           : customer_data.id_number.toString()
       }
       Axios.post(process.env.VUE_APP_API+'/attendence/attend',post_data)
+    },
+    getResult(read){
+      Axios.get(process.env.VUE_APP_API+`/attendence/find/${read}`)
+        .then(response => {
+          var data = response.data;
+          this.customer = { 
+            id_number     : data.detail.id,
+            message       : data.message,
+            name          : data.detail.name.toUpperCase(),
+            phone_number  : data.detail.phone,
+            email         : data.detail.email,
+            register_date : data.detail.register_date,
+            sitting_zone  : data.detail.sitting,
+            valid         : data.detail.attend_status
+          };
+          this.$refs.ticketModal.click()
+      });
     },
     async onInit(promise) {
       try {
@@ -127,30 +154,43 @@ export default {
     }
   },
   mounted: function() {
-    // Axios.get(process.env.VUE_APP_API+"/attendence/find/4")
-    //   .then(response => {
-    //     var data = response.data;
-    //     this.customer = { 
-    //       id_number     : data.detail.id,
-    //       message       : data.message,
-    //       name          : data.detail.name,
-    //       phone_number  : data.detail.phone_number,
-    //       email         : data.detail.email,
-    //       register_date : data.detail.register_date,
-    //       sitting_zone  : data.detail.sitting_zone
-    //     };
-    // });
-    this.$refs.ticketModal.click()
+
   }
 };
 </script>
 
 
-<style>
+<style scoped>
 .btn {
   border-radius: 0;
 }
 .modal-dialog-centered {
   align-items: flex-end
+}
+
+.fullscreen {
+  position: fixed;
+  z-index: 1000;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+}
+
+.fullscreen-button {
+  background-color: white;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  margin: 1rem;
+}
+.fullscreen-button img {
+  width: 2rem;
+}
+.qrreader {
+  position: absolute;
+  z-index: 1001;
+  left: 10px;
+  top:10px
 }
 </style>
